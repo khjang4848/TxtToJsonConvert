ed3509000f4b82f3d8a9070cdc2ef5b56de4b2bd
KangHyun Jang <khjang4848@gmail.com>
2017-09-11 19:39:37 +0900

    파일 처리 오류 수정

--- CSVToJsonTest/Program.cs
+++ CSVToJsonTest/Program.cs
@@ -8,10 +8,11 @@ namespace CSVToJsonTest
 {
     class Program
     {
+        private static string _basePath = @"C:\MyWork\GitHub\TxtToJsonConvert\CSVToJsonTest\";
         static void PrintJsonKorFile(List<BaseMessageModel> baseMessageModels, 
             List<BaseTextModel> baseTextModels )
         {
-            string basePath = @"D:\CSVToJsonTest\CSVToJsonTest\";
+            string basePath = _basePath;
             ResultJsonModel resultModel = new ResultJsonModel();
 
             var tt = baseMessageModels.GroupBy(x => x.SubCode);
@@ -33,22 +34,47 @@ namespace CSVToJsonTest
 
             foreach (var row in baseTextModels)
             {
-                resultModel.TextModel.Add(row.Key, row.EnglishName);
+                resultModel.TextModel.Add(row.Key, row.KoreanName);
             }
 
-            File.WriteAllText(basePath + "result.json", JsonConvert.SerializeObject(resultModel, Formatting.Indented));
+            File.WriteAllText(basePath + "result_kor.json", JsonConvert.SerializeObject(resultModel, Formatting.Indented));
         }
 
         static void PrintJsonEngFile(List<BaseMessageModel> baseMessageModels,
             List<BaseTextModel> baseTextModels)
         {
+            string basePath = _basePath;
             ResultJsonModel resultModel = new ResultJsonModel();
+
+            var tt = baseMessageModels.GroupBy(x => x.SubCode);
+
+            var dicc = new Dictionary<string, Dictionary<string, object>>();
+
+            foreach (var row in tt)
+            {
+                Dictionary<string, object> subModel = new Dictionary<string, object>();
+                foreach (var name in row)
+                {
+                    subModel.Add(name.MainCode, name.EnglishName);
+                }
+
+                dicc.Add(row.Key, subModel);
+            }
+
+            resultModel.Message = dicc;
+
+            foreach (var row in baseTextModels)
+            {
+                resultModel.TextModel.Add(row.Key, row.EnglishName);
+            }
+
+            File.WriteAllText(basePath + "result_eng.json", JsonConvert.SerializeObject(resultModel, Formatting.Indented));
 
         }
 
         static void Main(string[] args)
         {
-            string basePath = @"D:\CSVToJsonTest\CSVToJsonTest\";
+            string basePath = _basePath;
             string strFile = basePath + "message.txt";
             
             List<BaseMessageModel> testMessageList = new List<BaseMessageModel>();
@@ -63,6 +89,8 @@ namespace CSVToJsonTest
                     while ((strLineValue = sr.ReadLine()) != null)
                     {
                         if (string.IsNullOrEmpty(strLineValue)) return;
+
+                        strLineValue = strLineValue.Replace("\"", "");
 
                         values = strLineValue.Split('\t');
 
@@ -98,6 +126,8 @@ namespace CSVToJsonTest
                     {
                         if (string.IsNullOrEmpty(strLineValue)) return;
 
+                        strLineValue = strLineValue.Replace("\"", "");
+
                         values = strLineValue.Split('\t');
 
                         if (values[0] == "Key") continue;
@@ -120,6 +150,7 @@ namespace CSVToJsonTest
 
 
             PrintJsonKorFile(testMessageList, testModelList);
+            PrintJsonEngFile(testMessageList, testModelList);
 
 
 
